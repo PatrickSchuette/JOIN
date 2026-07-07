eba59affd498601e8623383dc58fd9f19d7d84b9
Patrick Schütte <patrick.sch@web.de>
2026-07-07 19:53:31 +0200

    Initialize boardPos for undefined status values

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -163,6 +163,9 @@ function getTaskArr(ALL_TASKS) {
             assignedTo: value.assignedTo,
             status: statusValue
         });
+        if (!boardPos[statusValue]) {
+            boardPos[statusValue] = {};
+        }
         boardPos[statusValue][key] = posValue;
     }
     return arr;
